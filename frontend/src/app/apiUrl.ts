d8d8e2199b99705f0278cd4d81135d097c4f8532
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-09-04 16:08:42 -0300

    Updated Dockerfile

--- frontend/src/app/apiUrl.ts
+++ frontend/src/app/apiUrl.ts
@@ -1,3 +1,1 @@
-export const apiUrl =
-  process.env.NEXT_PUBLIC_API_URL ||
-  (typeof window !== "undefined" ? "http://localhost:5000" : "");
+export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
