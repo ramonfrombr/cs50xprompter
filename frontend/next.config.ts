0f36cb2c66832d1f8031ddc761eeb6bbe0fe6518
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-09-04 18:19:21 -0300

    Updated render.yaml

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
 };
 
 export default nextConfig;
