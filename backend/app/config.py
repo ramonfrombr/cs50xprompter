5a780ebceb937ec9bb725658fe96b6915432d4b5
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-09-04 15:30:55 -0300

    Updated Dockerfile

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
