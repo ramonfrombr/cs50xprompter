a81f84d455930e078bdc4f063623a9717259d2d4
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-12-17 23:35:02 -0300

    Updated Dockerfile and wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,10 @@
+import os
 from app import create_app, socketio
-from app import db
 
 app = create_app("production")
 
 if __name__ == "__main__":
     import eventlet
     import eventlet.wsgi
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
